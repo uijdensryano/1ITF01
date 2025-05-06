2c08e290aed402018ee184dd2c70bbaf27ca6e76
uijdensryano <r1043412@student.thomasmore.be>
2025-05-06 15:42:41 +0200

    settings verder

--- Assets/EscapeMenu.cs
+++ Assets/EscapeMenu.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class EscapeMenu : MonoBehaviour
 {
+    /*
+    Known bugs:
+    Menu toggalable in settings
+    */
+    
     public GameObject CurrentMenu;
 
     public void QuitGame(){
